2effe98f950dd827fcdae30b3419b5de8815c7b9
Rin <82919982+Rin-chan@users.noreply.github.com>
2022-09-28 18:04:49 +0800

    Create Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Event_Ticket
+Based on https://www.figma.com/community/file/1153904318348130945
---
+++ src/styles/colors.js
@@ -0,0 +1,67 @@
+export const WHITE = '#FFFFFF';
+
+// Brand Color
+export const BRAND_COLOR_DARK_900 = '#070304';
+export const BRAND_COLOR_DARK_800 = '#090506';
+export const BRAND_COLOR_DARK_700 = '#0B0808';
+export const BRAND_COLOR_DARK_600 = '#0D0B0B';
+export const BRAND_COLOR_DEFAULT = '#101010';
+export const BRAND_COLOR_LIGHT_400 = '#6F6F6F';
+export const BRAND_COLOR_LIGHT_300 = '#B7B7B7';
+export const BRAND_COLOR_LIGHT_200 = '#E7E7E7';
+export const BRAND_COLOR_LIGHT_100 = '#F3F3F3';
+
+// Neutral
+export const NEUTRAL_DARK_900 = '#070304';
+export const NEUTRAL_DARK_800 = '#090506';
+export const NEUTRAL_DARK_700 = '#0B0808';
+export const NEUTRAL_DARK_600 = '#0D0B0B';
+export const NEUTRAL_DEFAULT = '#101010';
+export const NEUTRAL_LIGHT_400 = '#6F6F6F';
+export const NEUTRAL_LIGHT_300 = '#B7B7B7';
+export const NEUTRAL_LIGHT_200 = '#E7E7E7';
+export const NEUTRAL_LIGHT_100 = '#F3F3F3';
+
+// Success
+export const SUCCESS_DARK_900 = '#0F595A';
+export const SUCCESS_DARK_800 = '#1A6D66';
+export const SUCCESS_DARK_700 = '#298876';
+export const SUCCESS_DARK_600 = '#3BA285';
+export const SUCCESS_DEFAULT = '#52BD94';
+export const SUCCESS_LIGHT_400 = '#7BD7AB';
+export const SUCCESS_LIGHT_300 = '#9BEBBC';
+export const SUCCESS_LIGHT_200 = '#A1F9B4';
+export const SUCCESS_LIGHT_100 = '#CFFCD4';
+
+// Error
+export const ERROR_DARK_900 = '#7A0E40';
+export const ERROR_DARK_800 = '#931846';
+export const ERROR_DARK_700 = '#B7264F';
+export const ERROR_DARK_600 = '#DB3756';
+export const ERROR_DEFAULT = '#FF4C5E';
+export const ERROR_LIGHT_400 = '#FF7879';
+export const ERROR_LIGHT_300 = '#FF9C93';
+export const ERROR_LIGHT_200 = '#FFC3B7';
+export const ERROR_LIGHT_100 = '#FFE4DB';
+
+// Warning
+export const WARNING_DARK_900 = '#7A4B04';
+export const WARNING_DARK_800 = '#935F07';
+export const WARNING_DARK_700 = '#B77C0B';
+export const WARNING_DARK_600 = '#DB9B10';
+export const WARNING_DEFAULT = '#FFBD16';
+export const WARNING_LIGHT_400 = '#FFD250';
+export const WARNING_LIGHT_300 = '#FFDF73';
+export const WARNING_LIGHT_200 = '#FFECA1';
+export const WARNING_LIGHT_100 = '#FFF7D0';
+
+// Information
+export const INFORMATION_DARK_900 = '#071561';
+export const INFORMATION_DARK_800 = '#0D1F76';
+export const INFORMATION_DARK_700 = '#142D92';
+export const INFORMATION_DARK_600 = '#1D3EAF';
+export const INFORMATION_DEFAULT = '#2952CC';
+export const INFORMATION_LIGHT_400 = '#597FE0';
+export const INFORMATION_LIGHT_300 = '#7C9FEF';
+export const INFORMATION_LIGHT_200 = '#AAC4F9';
+export const INFORMATION_LIGHT_100 = '#D4E2FC';
